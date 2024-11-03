bd061a68534974dd13eacaeb542b6c280b7baaa6
Mathieu <bon.mathieu@gmail.com>
2024-11-03 23:30:31 +0100

    Added actual legal data in legalNoticeData.json. Fixed styling of LegalModal.

--- src/components/LegalModal.tsx
+++ src/components/LegalModal.tsx
@@ -1,11 +1,11 @@
 import React from "react"
+import legalNoticeData from "../data/legalNoticeData.json"
 
 interface LegalModalProps {
-	onClose: () => void // Prop to handle closing the modal
+	onClose: () => void
 }
 
 const LegalModal: React.FC<LegalModalProps> = ({ onClose }) => {
-	// Close modal if clicking outside content
 	const handleBackgroundClick = (e: React.MouseEvent<HTMLDivElement>) => {
 		if (e.target === e.currentTarget) {
 			onClose()
@@ -16,29 +16,23 @@ const LegalModal: React.FC<LegalModalProps> = ({ onClose }) => {
 		<div
 			className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50"
 			onClick={handleBackgroundClick}>
-			<div className="bg-white w-11/12 md:w-1/2 lg:w-1/3 p-6 rounded-lg shadow-lg relative">
-				<h2 className="text-2xl font-bold mb-4">Legal Notice</h2>
+			<div className="relative bg-white w-10/12 md:w-3/4 lg:w-3/4 max-w-[700px] max-h-screen p-6 rounded-lg shadow-lg overflow-y-auto">
+				<h2 className="text-xl font-bold mb-4">Legal Notice</h2>
 
-				<h3 className="text-lg font-semibold mt-4">Website Disclaimer</h3>
-				<p className="text-sm text-gray-600">
-					The content on this website is provided for informational purposes only and is
-					intended for a fictional portfolio demonstration. All examples, data, and
-					references to businesses are purely illustrative. Any resemblance to real
-					persons, living or dead, or real events is purely coincidental.
-				</p>
+				{legalNoticeData.map((item, index) => {
+					const [title, content] = Object.entries(item)[0]
+					return (
+						<div key={index} className="mt-4">
+							<h3 className="text-base font-semibold">{title}</h3>
+							<p className="text-xs text-gray-600">{content}</p>
+						</div>
+					)
+				})}
 
-				<h3 className="text-lg font-semibold mt-4">Copyright and Credits</h3>
-				<p className="text-sm text-gray-600">
-					All images are sourced from Unsplash and are licensed for free use. Icons and
-					graphics are provided by Heroicons under an open-source license. Credit is
-					extended to these creators for their resources. The source code for this site is
-					available on GitHub for educational purposes.
-				</p>
-
-				<div className="flex justify-center mt-6">
+				<div className="flex justify-center mt-6 sticky bottom-0 bg-white py-4">
 					<button
 						onClick={onClose}
-						className="inline-flex items-center justify-center w-full px-5 py-2 mr-2 text-m font-medium text-center text-gray-900 border bg-primary-2 border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
+						className="inline-flex items-center justify-center  px-8 py-2 mt-4 text-m font-medium text-center text-gray-900 border bg-primary-2 border-gray-200 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 transition duration-300">
 						Close
 					</button>
 				</div>
