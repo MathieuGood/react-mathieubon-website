cae36b554dbb6ab00778de3be2bb43e2d57b0a4a
Mathieu <bon.mathieu@gmail.com>
2024-11-03 23:39:00 +0100

    Replaced ProjectSeparator at the bottom of each project. Adjusted padding.

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -7,12 +7,12 @@ import ProjectSeparator from "./ProjectSeparator"
 const ProjectsSection: React.FC = () => {
 	return (
 		<section className="bg-gray-200">
-			<div className="max-w-screen-xl px-4 py-8 mx-auto space-y-12 md:py-20 lg:space-y-20 lg:py-24 lg:px-16">
+			<div className="max-w-screen-xl px-4 py-8 mx-auto space-y-12 md:pt-20 md:pb-10 lg:space-y-20 lg:pt-24 lg:pb-8 lg:px-16">
 				<h2 className="text-4xl font-bold text-primary-1">Projects</h2>
 
 				{projectsData.map((project, index) => (
 					<React.Fragment key={index}>
-						<ProjectSeparator />
+
 						<div className="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
 							{index % 2 === 0 ? (
 								<>
@@ -26,6 +26,7 @@ const ProjectsSection: React.FC = () => {
 								</>
 							)}
 						</div>
+						<ProjectSeparator />
 					</React.Fragment>
 				))}
 			</div>
