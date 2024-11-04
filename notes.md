9ec4146f2770b814c1cac44203f3fba4712efc39
Mathieu <bon.mathieu@gmail.com>
2024-11-04 01:49:51 +0100

    Added new project entry. Converted css styles to tailwind css classes in Terminal.

--- notes.md
+++ notes.md
@@ -1,6 +1,7 @@
 To do
 
 [ ] Fix avatar image size
+[ ] Fix Terminal whitespace issue
 [x] Fix project media layout on smaller screens
 [ ] Add more projects
 [ ] Add multilanguage support
--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -4,13 +4,13 @@ const Terminal: React.FC = () => {
 	const { commands, output } = terminalContentData[0]
 
 	return (
-		<div className="terminal mb-6">
-			<div className="terminal-header">
-				<div className="close"></div>
-				<div className="minimize"></div>
-				<div className="maximize"></div>
+		<div className="terminal mb-6 max-w-[650px] min-w-[200px] bg-[#2d2d2d] rounded-[5px] overflow-hidden text-[#c5c8c6] shadow-[0_0_10px_rgba(0,0,0,0.5)]">
+			<div className="terminal-header bg-[#e0e0e0] p-[10px] flex justify-start items-center">
+				<div className=" w-3 h-3 bg-red-400 rounded-full mr-[8px]"></div>
+				<div className=" w-3 h-3 bg-yellow-500 rounded-full mr-[8px]"></div>
+				<div className=" w-3 h-3 bg-green-500 rounded-full mr-[8px]"></div>
 			</div>
-			<div className="terminal-body">
+			<div className="terminal-body p-[20px] text-[16px] leading-[1.5] min-h-[220px]">
 				<div className="content text-xs lg:text-sm"></div>
 
 				{commands.map((commandLine, index) => (
